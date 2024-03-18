1e4bb007aa5c158c6d6a8f550678a8abbbdc01fa
shanon <shanonwambui24@gmail.com>
2024-03-18 09:34:11 +0300

    Updates

---
+++ precache-manifest.603cc16bfb50f50b4e48b50a8a5904e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36475c9611c47dda4338e4ce627d093f",
+    "url": "/dashboard-react/index.html"
+  },
+  {
+    "revision": "503b8193cc7fe8349fcf",
+    "url": "/dashboard-react/static/css/2.6a2bfea0.chunk.css"
+  },
+  {
+    "revision": "f243b234da815f06095c",
+    "url": "/dashboard-react/static/css/main.cf8c35ec.chunk.css"
+  },
+  {
+    "revision": "503b8193cc7fe8349fcf",
+    "url": "/dashboard-react/static/js/2.0ce1b542.chunk.js"
+  },
+  {
+    "revision": "f96b6412073fc243ced8aec712392d14",
+    "url": "/dashboard-react/static/js/2.0ce1b542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f243b234da815f06095c",
+    "url": "/dashboard-react/static/js/main.bba3a674.chunk.js"
+  },
+  {
+    "revision": "21aee61fb6e39368747c",
+    "url": "/dashboard-react/static/js/runtime-main.b0bd4e8b.js"
+  }
+]);
